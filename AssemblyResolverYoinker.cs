17a626db01a55c5824f05cb8974b42d179d9be44
Leonhard Seidel <leonhardseidel@gmx.de>
2025-03-27 22:16:06 +0100

    Add project files.

---
+++ AssemblyResolverYoinker.cs
@@ -0,0 +1,78 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace PS1PArty
+{
+    internal static class AssemblyResolverYoinker
+    {
+
+        public static void SetOurResolveHandlerAtFront()
+        {
+            BindingFlags flags = BindingFlags.Static | BindingFlags.NonPublic;
+            FieldInfo? field = null;
+
+            Type domainType = typeof(AssemblyLoadContext);
+
+            while (field is null)
+            {
+                if (domainType is not null)
+                {
+                    field = domainType.GetField("AssemblyResolve", flags);
+                }
+                else
+                {
+                    MelonLogger.Error("domainType got set to null for the AssemblyResolve event was null");
+                    return;
+                }
+                if (field is null)
+                {
+                    domainType = domainType.BaseType!;
+                }
+            }
+
+            MulticastDelegate resolveDelegate = (MulticastDelegate)field.GetValue(null)!;
+            Delegate[] subscribers = resolveDelegate.GetInvocationList();
+
+            Delegate currentDelegate = resolveDelegate;
+            for (int i = 0; i < subscribers.Length; i++)
+            {
+                currentDelegate = Delegate.RemoveAll(currentDelegate, subscribers[i])!;
+            }
+
+            Delegate[] newSubscriptions = new Delegate[subscribers.Length + 1];
+            newSubscriptions[0] = (ResolveEventHandler)AssemblyResolveEventListener!;
+            Array.Copy(subscribers, 0, newSubscriptions, 1, subscribers.Length);
+
+            currentDelegate = Delegate.Combine(newSubscriptions)!;
+
+            field.SetValue(null, currentDelegate);
+
+            //MelonLogger.Msg("Set our resolve handler at the front");
+        }
+
+        private static Assembly AssemblyResolveEventListener(object sender, ResolveEventArgs args)
+        {
+            if (args is null)
+            {
+                return null!;
+            }
+            string cleanName = args.Name[..args.Name.IndexOf(',')];
+            string name = "HPVR.Resources." + cleanName + ".dll";
+            //MelonLogger.Msg(cleanName + " -> " + name);
+            using Stream? str = Assembly.GetExecutingAssembly().GetManifestResourceStream(name);
+            if (str is not null)
+            {
+                var context = new AssemblyLoadContext(name, false);
+                string path = Path.Combine(Directory.GetParent(Assembly.GetExecutingAssembly()?.Location!)!.Parent!.FullName, "UserLibs", cleanName + ".dll");
+                FileStream fstr = new(path, FileMode.Create);
+                str.CopyTo(fstr);
+                fstr.Close();
+                str.Position = 0;
+
+                return context.LoadFromStream(str);
+            }
+            return null!;
+        }
+    }
+}
---
+++ PS1Party.cs
@@ -0,0 +1,166 @@
+﻿using Il2Cpp;
+using Il2CppInterop.Runtime;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.Rendering.HighDefinition.Compositor;
+using UnityEngine.UI;
+
+namespace PS1PArty
+{
+    public class PS1Party : MelonMod
+    {
+        RenderTexture? rt;
+        RawImage? image;
+        Canvas? canvas;
+        HDAdditionalCameraData? cameraData;
+        int scaledHeight, scaledWidth;
+
+        MelonPreferences_Entry<int>? Height;
+
+        static PS1Party()
+        {
+            AssemblyResolverYoinker.SetOurResolveHandlerAtFront();
+        }
+
+        public override void OnInitializeMelon()
+        {
+            var category = MelonPreferences.CreateCategory("PS1Party");
+            Height = category.CreateEntry("height", 224, "Height", description: "This it the virtual height of the game now. width scales accordingly");
+            MelonPreferences.Save();
+            float x = (float)Screen.height / (Height?.Value) ?? 224f;
+            scaledWidth = (int)((float)Screen.width / x);
+            scaledHeight = (int)((float)Screen.height / x);
+
+            MelonLogger.Msg($"Initializing with set height {Height?.Value ?? 224} => {scaledWidth}x{scaledHeight} instead of {Screen.width}x{Screen.height}");
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName != "Disclaimer")
+            {
+                //todo we have to set filtermode to point for all textures
+                foreach (var obj in UnityEngine.Object.FindObjectsOfTypeAll(Il2CppType.Of<Texture>()))
+                {
+                    var material = obj.TryCast<Texture>();
+
+                    if (material is not null)
+                    {
+                        material.filterMode = FilterMode.Point;
+                    }
+                }
+
+                GameObject CanvasGO = new()
+                {
+                    name = "lowres",
+                    layer = LayerMask.NameToLayer("UI")
+                };
+                canvas = CanvasGO.AddComponent<Canvas>();
+                canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+                //_ = CanvasGO.AddComponent<GraphicRaycaster>();
+                float x = Screen.height / Height!.Value;
+                scaledWidth = (int)(Screen.width / x);
+                scaledHeight = (int)(Screen.height / x);
+                rt = new RenderTexture(scaledWidth, scaledHeight, 16, RenderTextureFormat.ARGB4444)
+                {
+                    filterMode = FilterMode.Point,
+                    anisoLevel = 0,
+                    depthStencilFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.None,
+                    antiAliasing = 1 //means off
+                };
+                Camera.main.targetTexture = rt;
+                Camera.main.forceIntoRenderTexture = true;
+                Camera.main.cameraType = CameraType.SceneView;
+
+                GameObject ImageGO = new()
+                {
+                    name = "image",
+                    layer = LayerMask.NameToLayer("UI")
+                };
+                ImageGO.transform.parent = CanvasGO.transform;
+                image = ImageGO.AddComponent<RawImage>();
+                image.texture = rt;
+                ImageGO.transform.localPosition = Vector3.zero;
+                ImageGO.transform.localScale = new(x, x, x);
+                image.rectTransform.sizeDelta = new(rt.width, rt.height);
+
+                //set some more camera settings
+                cameraData = Camera.main.GetComponent<HDAdditionalCameraData>();
+                var frameSettings = cameraData.m_RenderingPathCustomFrameSettings;
+                frameSettings.SetEnabled(FrameSettingsField.Bloom, false);
+                frameSettings.SetEnabled(FrameSettingsField.ChromaticAberration, false);
+                frameSettings.SetEnabled(FrameSettingsField.Vignette, false);
+                frameSettings.SetEnabled(FrameSettingsField.FilmGrain, false);
+                frameSettings.SetEnabled(FrameSettingsField.ShadowMaps, false);
+                frameSettings.SetEnabled(FrameSettingsField.SSAO, false);
+                frameSettings.SetEnabled(FrameSettingsField.SSGI, false);
+                frameSettings.SetEnabled(FrameSettingsField.ColorGrading, false);
+                frameSettings.SetEnabled(FrameSettingsField.DirectSpecularLighting, false);
+                frameSettings.SetEnabled(FrameSettingsField.SubsurfaceScattering, false);
+                frameSettings.SetEnabled(FrameSettingsField.Dithering, false);
+                frameSettings.SetEnabled(FrameSettingsField.Antialiasing, false);
+                frameSettings.SetEnabled(FrameSettingsField.MSAA, false);
+                frameSettings.SetEnabled(FrameSettingsField.ContactShadows, false);
+                frameSettings.SetEnabled(FrameSettingsField.Shadowmask, false);
+                frameSettings.SetEnabled(FrameSettingsField.SSAOAsync, false);
+                frameSettings.materialQuality = UnityEngine.Rendering.MaterialQuality.Low;
+                cameraData.m_RenderingPathCustomFrameSettings = frameSettings;
+
+                Camera.main.allowMSAA = false;
+                Camera.main.allowDynamicResolution = false;
+                cameraData.allowDynamicResolution = false;
+                cameraData.antialiasing = HDAdditionalCameraData.AntialiasingMode.None;
+                cameraData.customRenderingSettings = true;
+                rt.antiAliasing = 1;
+                QualitySettings.antiAliasing = 0;
+                QualitySettings.lodBias = 0.001f;
+                QualitySettings.shadows = ShadowQuality.Disable;
+                QualitySettings.globalTextureMipmapLimit = 4;
+                QualitySettings.anisotropicFiltering = AnisotropicFiltering.Disable;
+                CanvasGO.transform.position = new(CanvasGO.transform.position.x, CanvasGO.transform.position.y, 100);
+
+                if (sceneName == "GameMain")
+                {
+                    var volumeGO = GameObject.Find("Global Volume");
+                    var volume = volumeGO?.GetComponent<Volume>();
+                    if (volume is not null)
+                    {
+                        foreach (var item in volume.profile.components)
+                        {
+                            if (item.name.StartsWith("Exposure"))
+                            {
+                                for (int i = 0; i < item.parameterList.Count; i++)
+                                {
+                                    if (item.parameterList[i].GetIl2CppType() == Il2CppType.Of<AdaptationModeParameter>())
+                                    {
+                                        var param = item.parameterList[i].Cast<AdaptationModeParameter>();
+                                        param.value = AdaptationMode.Fixed;
+                                        item.parameterList[i] = param;
+                                        break;
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                foreach (var obj in UnityEngine.Object.FindObjectsOfTypeAll(Il2CppType.Of<Canvas>()))
+                {
+                    var canvas = obj.TryCast<Canvas>();
+
+                    if (canvas is not null)
+                    {
+                        if (canvas.name != "lowres")
+                        {
+                            canvas.enabled = !canvas.enabled;
+                            canvas.enabled = !canvas.enabled;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
